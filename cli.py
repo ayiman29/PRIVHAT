e8dddfd8f4bad28b2d028626e7d327d5b5179abc
RZ Ayiman <ayimaneg@gmail.com>
2025-06-13 16:35:57 +0600

    cli.py and crypto tools added

--- cli.py
+++ cli.py
@@ -1,1 +1,65 @@
 
+# cli.py
+import argparse
+from user_manager import create_user
+from crypto_engine import encrypt_message, decrypt_message, sign_message, verify_signature
+
+def main():
+    parser = argparse.ArgumentParser(prog='privhat', description='Cryptography CLI Tool')
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Create user
+    parser_create = subparsers.add_parser('create-user')
+    parser_create.add_argument('username')
+    parser_create.add_argument('--alg', choices=['rsa', 'ecc', 'elgamal'], required=True)
+
+    # Encrypt
+    parser_encrypt = subparsers.add_parser('encrypt')
+    parser_encrypt.add_argument('--to', required=True)
+    parser_encrypt.add_argument('--in', dest='infile', required=True)
+    parser_encrypt.add_argument('--out', required=True)
+    parser_encrypt.add_argument('--alg', choices=['rsa', 'ecc', 'elgamal', 'hybrid'], required=True)
+
+    # Decrypt
+    parser_decrypt = subparsers.add_parser('decrypt')
+    parser_decrypt.add_argument('--user', required=True)
+    parser_decrypt.add_argument('--in', dest='infile', required=True)
+    parser_decrypt.add_argument('--out', required=True)
+
+    # Sign
+    parser_sign = subparsers.add_parser('sign')
+    parser_sign.add_argument('--user', required=True)
+    parser_sign.add_argument('--in', dest='infile', required=True)
+    parser_sign.add_argument('--out', required=True)
+    parser_sign.add_argument('--alg', choices=['rsa', 'ecdsa'], required=True)
+
+    # Verify
+    parser_verify = subparsers.add_parser('verify')
+    parser_verify.add_argument('--from', dest='username', required=True)
+    parser_verify.add_argument('--in', dest='infile', required=True)
+    parser_verify.add_argument('--sig', required=True)
+    parser_verify.add_argument('--alg', choices=['rsa', 'ecdsa'], required=True)
+
+    args = parser.parse_args()
+
+    if args.command == 'create-user':
+        create_user(args.username, args.alg)
+
+    elif args.command == 'encrypt':
+        encrypt_message(args.to, args.infile, args.out, args.alg)
+
+    elif args.command == 'decrypt':
+        decrypt_message(args.user, args.infile, args.out)
+
+    elif args.command == 'sign':
+        sign_message(args.user, args.infile, args.out, args.alg)
+
+    elif args.command == 'verify':
+        verify_signature(args.username, args.infile, args.sig, args.alg)
+
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,1 +1,70 @@
+import random
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+    
+    
+def extended_gcd_iterative(a, b):
+    x0, x1 = 1, 0
+    y0, y1 = 0, 1
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return (a, x0, y0)
 
+def modinv(a, m):
+    g, x, y = extended_gcd_iterative(a, m)
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    return x % m
+
+
+def modexp(base, exp, mod):
+    result = 1
+    base = base % mod 
+
+    while exp > 0:
+        if exp % 2 == 1:
+            result = (result * base) % mod
+        base = (base * base) % mod
+        exp = exp // 2
+    
+    return result
+
+def is_prime(n, k=5):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0:
+        return False
+
+    r, d = 0, n - 1
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = modexp(a, d, n)  
+        if x == 1 or x == n - 1:
+            continue
+        
+        for _ in range(r - 1):
+            x = (x * x) % n
+            if x == n - 1:
+                break
+        else:
+            return False  
+    
+    return True  
+
+def int_to_text(number):
+    byte_length = (number.bit_length() + 7) // 8
+    return number.to_bytes(byte_length, byteorder='big').decode('utf-8')
+
+def text_to_int(text):
+    return int.from_bytes(text.encode('utf-8'), byteorder='big')
